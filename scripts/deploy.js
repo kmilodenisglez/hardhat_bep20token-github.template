3271d46fb88b23e96f67c1e8404696c7a488a683
kmilo <kmilo.denis.glez@yandex.com>
2025-04-08 10:43:41 -0400

    fix

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,23 +1,24 @@
 async function main() {
     const [deployer] = await ethers.getSigners();
-  
-    console.log("Deploying contracts with the account:", deployer.address);
-  
-    console.log("Account balance:", (await deployer.getBalance()).toString());
 
-    const Token = await ethers.getContractFactory("KDG"); //Replace with name of your smart contract
+    console.log("Deploying contracts with the account:", deployer.address);
+
+    // Obtén el balance usando el provider
+    const balance = await ethers.provider.getBalance(deployer.address);
+    console.log("Account balance:", balance.toString());
+
+    const Token = await ethers.getContractFactory("KDG");
     const myToken = await Token.deploy();
 
-    await myToken.deployed();
+    await myToken.waitForDeployment();
 
     console.log("Token Successfully Deployed!");
-    console.log("Token address:", myToken.address);
-  }
-  
-  main()
+    console.log("Token address:", await myToken.getAddress());
+}
+
+main()
     .then(() => process.exit(0))
     .catch((error) => {
-      console.error(error);
-      process.exit(1);
+        console.error(error);
+        process.exit(1);
     });
-  
